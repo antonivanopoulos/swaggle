1bf2c0f7bd6ea4befef0885a8357251f139e8610
Chris Jewell <chrisjohnjewell@gmail.com>
2016-10-14 07:49:01 +1100

    Change tests to assert that the path we expect is in the list

--- test/path_parser_test.exs
+++ test/path_parser_test.exs
@@ -2,24 +2,24 @@ defmodule PathParserTest do
   use ExUnit.Case
   doctest Swaggle
 
-  test "We can find the first path" do
-    paths = Swaggle.PathParser.get_paths("test/fixtures/petstore-api.yml")
-    path = paths |> Enum.fetch!(0)
-
-    assert path == "/api/v1/pets"
-  end
-
-  test "It varies by file" do
+  test "parses path from beers file" do
     paths = Swaggle.PathParser.get_paths("test/fixtures/beers-api.yml")
-    path = paths |> Enum.fetch!(0)
 
-    assert path == "/api/v1/beers"
+    assert Enum.member?(paths, "/api/v1/beers")
   end
 
-  test "I can think of a third api" do
+  test "parses path from restaurants file" do
     paths = Swaggle.PathParser.get_paths("test/fixtures/restaurants-api.yml")
-    path = paths |> Enum.fetch!(0)
 
-    assert path == "/api/v1/restaurants"
+    assert Enum.member?(paths, "/api/v1/restaurants")
+  end
+
+  test "collects multiple paths" do
+    paths = Swaggle.PathParser.get_paths("test/fixtures/pets-api.yml")
+    expected_paths = ["/api/v1/pets", "/api/v1/owners", "/api/v1/breeds"]
+
+    Enum.each(paths, fn (path) ->
+      assert Enum.member?(expected_paths, path)
+    end)
   end
 end
